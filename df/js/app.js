b5281d1103707f6c2a9977f19cd78b0d0e736654
zhoubing <418650386@qq.com>
2015-01-10 17:46:32 +0800

    增加第三部分

--- df/js/app.js
+++ df/js/app.js
@@ -1,3 +1,87 @@
+var arrP2={
+    sm:["国企改革试点扩大，混合所有制，薪酬改革，打破旧制度藩篱，催生市场热点",
+        "上海自贸区珠玉在前，第二批自贸区牌照待发，津闽粤相关个股存在主题投资机会",
+        "土地管理法与中央1号文件主题双重叠加效应",
+        "体制改革加速，长期限制体育产业发展的政企不分因素，有望打破，催生新业态、新经济和新投资热点",
+        "迪斯尼2015年底落成，利好周边消费、旅游等",
+        "中国从韬光养晦走向资本输出，区域经济令人遐想",
+        "新型城镇化规划出台第二年，政策细化催生相关概念",
+        "智能化、信息化，中国经济长期依赖的制造业获得产业升级机会",
+        "城市功能重新定位，河北经济提速，京津战略转型"],
+    yp:["15年一季度看央企，二季度地方国企","15年一季度","15年年初","15年一季度","14年四季度起至迪斯尼落成前夕结束",
+        "14年四季度——15年一季度","贯穿全年","贯穿全年","15年上半年"],
+    gg:["百联股份、浦东金桥","北部湾港、浦东金桥、自仪股份","杭钢集团、浦东金桥、华西股份","中体产业、雷曼光电、探路者",
+        "百联股份、浦东金桥、锦江股份","青松建化、八一钢铁、上港集团、宁波港","地产、基建、智慧城市","机器人、汇川技术、长荣股份、光韵达","荣盛发展、廊坊发展、华夏幸福"]
+};
+var arr31={
+    sy:["集成电路、液晶面板","连接器、机壳、电感、电池","可穿戴、智能家居","地方政府、大型集团、高校上市公司平台"],
+    tj:["通富微电、华天科技、彩虹股份、南洋科技","立讯精密、长盈精密、顺络电子、欣旺达","英唐智控、北京君正、华天科技、歌尔声学","上海贝岭、东湖高新、华工科技"]
+};
+var arr32={
+    sy:["金融信息化、互联网金融、移动支付","信息安全、自主可控、云计算","农业信息化、食品溯源","医疗信息化、健康管理与服务","物流信息化"],
+    tj:["安硕信息、长亮科技、中科金财、信雅达、润和软件、金证股份、恒生电子、新大陆","天玑科技、卫士通、东方通、绿盟科技、启明星辰、中国软件、浪潮信息","神州信息、华宇软件、飞利信","卫宁软件、东软集团、荣科科技、易联众","中海科技、皖通科技、久其软件"]
+};
+var arr33={
+    sy:["通信设备、运维、网优","网优","运营商、部分国企设备商"],
+    tj:["中兴通讯","宜通世纪、世纪鼎利","鹏博士、高鸿股份、中国联通"]
+};
+var arr34={
+    sy:["出版传媒、有线电视网络","皖新传媒、中文传媒、中南传媒、凤凰传媒、歌华有线、人民网","数字营销、互联网医疗/教育、电视节目"],
+    tj:["体育赛事、体育媒体/广告","中体产业、雷曼光电、强生控股、乐视网、信隆实业、广弘控股","天龙集团、明家科技、蓝色光标、省广股份、华策影视、华录百纳、朗玛信息、奥飞动漫"]
+};
+var arr35={
+    sy:["信息化","直升机、大飞机","可穿戴、智能家居","重组预期标的，民营标的"],
+    tj:["闽福发A、通光线缆、航天科技、四川九洲","哈飞股份、中航飞机","四创电子、国睿科技、通光线缆"]
+};
+var arr36={
+    sy:["饮料","大众食品","可穿戴、智能家居","大众食品及饮料","整体"],
+    tj:["山西汾酒，三全食品，张裕A","光明乳业，承德露露","好想你，三全食品，百润股份，金枫酒业","老白干酒，惠泉啤酒，上海梅林，金枫酒业，中炬高新"]
+};
+var arr37={
+    sy:["棉纺","男装","全板块"],
+    tj:["鲁泰A、华孚色纺、百隆东方","七匹狼、九牧王等男装公司，包括连带其他品牌服饰","奥康国际、森马服饰、棒杰股份、嘉欣丝绸、兴业科技、卡奴迪路、乔治白等"]
+};
+var arr38={
+    sy:["化学药板块","医疗器械板块","生物药板块","医疗服务"],
+    tj:["恒瑞医药、恩华药业、华东医药","华润万东、戴维医疗","海正药业、兰生股份","金陵药业、爱尔眼科、开元投资"]
+};
+var arr39={
+    sy:["海参养殖","饲料加工","畜禽养殖","种业","制糖业"],
+    tj:["壹桥苗业","新希望、海大集团、唐人神、大北农","圣农发展、ST民和、牧原股份、雏鹰农牧、益生股份","登海种业","南宁糖业、中粮屯河"]
+};
+var arr310={
+    tj:["锦旅B股、锦江股份、锦江酒店、中青旅、宋城演艺","锦江股份、锦江酒店、全聚德、中国国旅","桂林旅游","携程、中青旅","锦江股份、宋城演艺"]
+};
+/**/
+var arr311={
+    sy:["房地产营销"],
+    tj:["招商地产、万科A、华夏幸福","国投中鲁等","世联行、三六五网","南国置业、中华企业","暂无明确标的"]
+};
+var arr312={
+    sy:["证券","证券","保险","保险"],
+    tj:["国元证券，光大证券","东吴证券，锦龙股份","中国太保","中国平安，新华保险"]
+};
+var arr313={
+    sy:["工业4.0、汽车拆解、高铁、水表、通航","通用机械、养路机械、工具等",
+        "工程机械、冷链设备、工业车辆等","核心零部件","节能装备"],
+    tj:["新松机器人、天奇股份、华昌达、亚威股份、博深工具、三川股份、新天科技、威海广泰",
+        "南方泵业、隆鑫通用、隆华节能、森远股份、博深工具、巨星科技",
+        "安徽合力、柳工、大冷股份、一拖股份",
+        "恒立油缸、博深工具",
+        "隆华节能、开山股份"]
+};
+var arr314={
+    sy:["零部件","整车、零部件、电池及上游","整车","轻卡及零部件"],
+    tj:["隆基机械、金固股份、德联集团","江淮汽车、宇通汽车、松芝股份、均胜电子","江淮汽车、上汽集团、广汽集团","江淮汽车、江铃汽车"]
+};
+var arr315={
+    sy:["天然气","丙烷脱氢"],
+    tj:["胜利股份","万华化学"]
+};
+var arr316={
+    sy:["光伏、风电","光伏","储能","核电","风电"],
+    tj:["粤水电、云南盐化、福建南纺、甘肃电投","阳光电源、隆基股份","阳光电源","丹甫股份","金风科技、吉新科技"]
+};
 $(document).ready(function(){
     var height = screen.height;
     var size = $(".swiper-wrapper .item").size();
@@ -7,6 +91,14 @@ $(document).ready(function(){
         mode: 'vertical',
         loop:false,
         grabCursor: true
+    });
+    var part3Swiper = new Swiper('#part3-container',{
+        mode: 'horizontal',
+        loop:false,
+        grabCursor: true,
+        onSlideChangeEnd:function(swiper){
+            console.log(swiper.activeIndex);
+        }
     });
     setTimeout(function(){
         $(".swiper-slide").addClass("visiable");
@@ -20,4 +112,119 @@ $(document).ready(function(){
     $(".cyli-lst").find(".z-niu").remove(); 
 	$(".cyli-lst li").removeClass("active").eq(index).addClass("active").append($niu);
 
+}).on("click",".slide31 .node",function(){
+    var acNode = $(this).closest("div").children(".node-ac");
+    var oldIndex = $(this).closest("div").attr("data-index");
+    var index = $(this).attr("data-index");
+    acNode.children("h3").html(acNode.attr("p"+index));
+    acNode.removeClass("node"+oldIndex).addClass("node"+index);
+    $(this).removeClass("node"+index).addClass("node"+oldIndex);
+    $(this).attr("data-index",oldIndex);
+    $(this).closest("div").attr("data-index",index);
+    switch(index){
+        case "1":
+        $(".cont-31 li").removeClass("active").addClass("active");
+        $(".wrap-cont-txt31").removeClass($(".wrap-cont-txt31").attr("data-class")).addClass("w100").attr("data-class","w100");
+        $(".wrap-cont-txt31").find(".cont-txt1").html('<span class="cont-name">受益板块：</span>'+arr31.sy[0]);
+        $(".wrap-cont-txt31").find(".cont-txt2").html('<span class="cont-name">推荐公司：</span>'+arr31.tj[0]);
+        break;
+        case "2":
+        $(".cont-31 li").removeClass("active").eq(0).addClass("active");
+        $(".wrap-cont-txt31").removeClass($(".wrap-cont-txt31").attr("data-class")).addClass("w25").attr("data-class","w25");
+        $(".wrap-cont-txt31").find(".cont-txt1").html('<span class="cont-name">受益板块：</span>'+arr31.sy[1]);
+        $(".wrap-cont-txt31").find(".cont-txt2").html('<span class="cont-name">推荐公司：</span>'+arr31.tj[1]);
+        break;
+        case "3":
+        $(".cont-31 li").removeClass("active");
+        $(".cont-31 li").eq(0).addClass("active");
+        $(".cont-31 li").eq(1).addClass("active");
+        $(".wrap-cont-txt31").removeClass($(".wrap-cont-txt31").attr("data-class")).addClass("w50").attr("data-class","w50");
+        $(".wrap-cont-txt31").find(".cont-txt1").html('<span class="cont-name">受益板块：</span>'+arr31.sy[2]);
+        $(".wrap-cont-txt31").find(".cont-txt2").html('<span class="cont-name">推荐公司：</span>'+arr31.tj[2]);
+        break;
+        case "4":
+        $(".cont-31 li").removeClass("active");
+        $(".cont-31 li").eq(2).addClass("active");
+        $(".cont-31 li").eq(3).addClass("active");
+        $(".wrap-cont-txt31").removeClass($(".wrap-cont-txt31").attr("data-class")).addClass("w50 left50").attr("data-class","w50 left50");
+        $(".wrap-cont-txt31").find(".cont-txt1").html('<span class="cont-name">受益板块：</span>'+arr31.sy[3]);
+        $(".wrap-cont-txt31").find(".cont-txt2").html('<span class="cont-name">推荐公司：</span>'+arr31.tj[3]);
+        break;
+    }
+}).on("click",".slide32 .node",function(){
+    var acNode = $(this).closest("div").children(".node-ac");
+    var oldIndex = $(this).closest("div").attr("data-index");
+    var index = $(this).attr("data-index");
+    acNode.children("h3").html(acNode.attr("p"+index));
+    acNode.removeClass("node_"+oldIndex).addClass("node_"+index);
+    $(this).removeClass("node_"+index).addClass("node_"+oldIndex);
+    $(this).attr("data-index",oldIndex);
+    $(this).closest("div").attr("data-index",index);
+    switch(index){
+        case "1":
+        $(".cont-32 li").removeClass("active");
+        $(".cont-32 li").eq(0).addClass("active");
+        $(".cont-32 li").eq(1).addClass("active");
+        $(".wrap-cont-txt32").removeClass($(".wrap-cont-txt32").attr("data-class")).addClass("w50").attr("data-class","w50");
+        $(".wrap-cont-txt32").find(".cont-txt1").html('<span class="cont-name">受益板块：</span>'+arr32.sy[0]);
+        $(".wrap-cont-txt32").find(".cont-txt2").html('<span class="cont-name">推荐公司：</span>'+arr32.tj[0]);
+        break;
+        case "2":
+        $(".cont-32 li").removeClass("active").eq(2).addClass("active");
+        $(".wrap-cont-txt32").removeClass($(".wrap-cont-txt32").attr("data-class")).addClass("w25 left50").attr("data-class","w25 left50");
+        $(".wrap-cont-txt32").find(".cont-txt1").html('<span class="cont-name">受益板块：</span>'+arr32.sy[1]);
+        $(".wrap-cont-txt32").find(".cont-txt2").html('<span class="cont-name">推荐公司：</span>'+arr32.tj[1]);
+        break;
+        case "3":
+        $(".cont-32 li").removeClass("active").eq(0).addClass("active");
+        $(".wrap-cont-txt32").removeClass($(".wrap-cont-txt32").attr("data-class")).addClass("w25").attr("data-class","w25");
+        $(".wrap-cont-txt32").find(".cont-txt1").html('<span class="cont-name">受益板块：</span>'+arr32.sy[2]);
+        $(".wrap-cont-txt32").find(".cont-txt2").html('<span class="cont-name">推荐公司：</span>'+arr32.tj[2]);
+        break;
+        case "4":
+        $(".cont-32 li").removeClass("active");
+        $(".cont-32 li").eq(0).addClass("active");
+        $(".cont-32 li").eq(1).addClass("active");
+        $(".wrap-cont-txt32").removeClass($(".wrap-cont-txt32").attr("data-class")).addClass("w50").attr("data-class","w50");
+        $(".wrap-cont-txt32").find(".cont-txt1").html('<span class="cont-name">受益板块：</span>'+arr32.sy[3]);
+        $(".wrap-cont-txt32").find(".cont-txt2").html('<span class="cont-name">推荐公司：</span>'+arr32.tj[3]);
+        break;
+        case "5":
+        $(".cont-32 li").removeClass("active");
+        $(".cont-32 li").eq(0).addClass("active");
+        $(".cont-32 li").eq(1).addClass("active");
+        $(".wrap-cont-txt32").removeClass($(".wrap-cont-txt32").attr("data-class")).addClass("w50").attr("data-class","w50");
+        $(".wrap-cont-txt32").find(".cont-txt1").html('<span class="cont-name">受益板块：</span>'+arr32.sy[4]);
+        $(".wrap-cont-txt32").find(".cont-txt2").html('<span class="cont-name">推荐公司：</span>'+arr32.tj[4]);
+        break;
+    }
+}).on("click",".slide33 .node",function(){
+    var acNode = $(this).closest("div").children(".node-ac");
+    var oldIndex = $(this).closest("div").attr("data-index");
+    var index = $(this).attr("data-index");
+    acNode.children("h3").html(acNode.attr("p"+index));
+    acNode.removeClass("node-"+oldIndex).addClass("node-"+index);
+    $(this).removeClass("node-"+index).addClass("node-"+oldIndex);
+    $(this).attr("data-index",oldIndex);
+    $(this).closest("div").attr("data-index",index);
+    switch(index){
+        case "1":
+        $(".cont-33 li").removeClass("active").eq(0).addClass("active");
+        $(".wrap-cont-txt33").removeClass($(".wrap-cont-txt33").attr("data-class")).addClass("w25").attr("data-class","w25");
+        $(".wrap-cont-txt33").find(".cont-txt1").html('<span class="cont-name">受益板块：</span>'+arr33.sy[0]);
+        $(".wrap-cont-txt33").find(".cont-txt2").html('<span class="cont-name">推荐公司：</span>'+arr33.tj[0]);
+        break;
+        case "2":
+        $(".cont-33 li").removeClass("active").addClass("active");
+        $(".wrap-cont-txt33").removeClass($(".wrap-cont-txt33").attr("data-class")).addClass("w100").attr("data-class","w100");
+        $(".wrap-cont-txt33").find(".cont-txt1").html('<span class="cont-name">受益板块：</span>'+arr33.sy[1]);
+        $(".wrap-cont-txt33").find(".cont-txt2").html('<span class="cont-name">推荐公司：</span>'+arr33.tj[1]);
+        break;
+        case "3":
+        $(".cont-33 li").removeClass("active").eq(0).addClass("active");
+        $(".wrap-cont-txt33").removeClass($(".wrap-cont-txt33").attr("data-class")).addClass("w25").attr("data-class","w25");
+        $(".wrap-cont-txt33").find(".cont-txt1").html('<span class="cont-name">受益板块：</span>'+arr33.sy[2]);
+        $(".wrap-cont-txt33").find(".cont-txt2").html('<span class="cont-name">推荐公司：</span>'+arr33.tj[2]);
+        break;      
+    }
 });
